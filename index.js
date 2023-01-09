047a372596a5b42bb1efc2d1aebcfbc8b2c156ad
Maansi <s3885529@student.rmit.edu.au>
2023-01-09 20:03:22 +1100

    side scroll services

--- index.js
+++ index.js
@@ -13,3 +13,32 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 4,
+  spaceBetween: 25,
+  sliderPerGroup: 4,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+    },
+  },
+});
