d470be45cbb88ecc4a68fbde2fb9f19d105402d0
Maansi <s3885529@student.rmit.edu.au>
2023-01-11 03:13:05 +1100

    contact page base

---
+++ js/contact.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
