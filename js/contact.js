dc17f31a3c84d05449c8fab06dfc118883fc2462
Maansi <s3885529@student.rmit.edu.au>
2023-01-11 11:51:36 +1100

    contact us validation

--- js/contact.js
+++ js/contact.js
@@ -13,3 +13,39 @@ function myFunction() {
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
+  //this is the main function 
+function checkForm() {
+
+    //collecting user inputs 
+    fullname = document.getElementById("fullname").value;
+    email = document.getElementById("email").value;
+     //calls another function checkEmail
+    message = document.getElementById("message").value;
+  if (fullname == "") 
+    { 
+
+	document.getElementById("fullnameError"). style.display = "inline";
+	document.getElementById("fullname").select();
+	document.getElementById("fullname").focus();
+	return false;
+    }
+  else if (email == "") 
+    {
+   
+   document.getElementById("emailError"). style.display = "inline";
+   document.getElementById("email").select();
+   document.getElementById("email").focus();
+   return false;
+     }
+     else if (message == "") 
+     {
+    
+    document.getElementById("messageError"). style.display = "inline";
+    document.getElementById("email").select();
+    document.getElementById("email").focus();
+    return false;
+      }
+
+alert("Submitted!");
+return true;
+}
