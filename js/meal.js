2ab78612cacf22a7ebdff6608c6b995af22f690e
Maansi <s3885529@student.rmit.edu.au>
2023-01-14 17:39:44 +1100

    meal planner

---
+++ js/meal.js
@@ -0,0 +1,168 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+  function calcCal () {
+    // (A) Get elements
+    let calorie = parseInt(document.getElementById("calorie").value),
+    results = document.getElementById("calorie-results");
+
+
+
+var food = {
+    "Acai Bowl with Fruits" : 250,
+    "Masala Omlette" : 212,
+    "Choco Pancakes with Stawberries" : 275, 
+    "Blueberry Smoothie" : 172,
+    "Sausage and Egg Sandwiches" : 298,
+    "Black Bean soup" : 550,
+    "Chicken and Farro Salad": 650,
+    "Pot Beef Curry with Rice" : 574,
+    "Bolognese Pasta" : 520,
+    "Pork Lettuce Wraps" : 425,
+    "Rainbow Sushi" : 320,
+    "Stuffed Butternut Squash" : 435,
+    "Straberry and Cream Granola Bar" : 290 , 
+    "BLT Eggs" : 310,
+    "PB Choco Cookie" : 225,
+    "Corn Zucchini Fritters" : 280,
+    "Prezel" : 150,
+}
+
+
+var length = Object.keys(food).length
+var count = 0
+
+var food_list = [{}]
+
+for (let i = 0; i < (length - 1); i++) { 
+
+    if (calorie - count >= 50){
+
+        let index = Math.floor(Math.random() * 8)
+        console.log(index)
+
+        var chosen_food = Object.keys(food)[index]    
+        var chosen_food_value = food[chosen_food]
+
+        if (chosen_food_value - (calorie-count)<=50){
+            food_list.push({key: chosen_food, value: food[chosen_food]});
+            count = count + chosen_food_value
+        }
+    }
+    
+}
+
+
+  
+  
+  food_list = food_list.slice(1)
+  console.log(food_list)
+  //results.innerHTML = JSON.stringify(food_list)
+  results.innerHTML = ''
+  for (let j = 0; j < food_list.length; j++){
+    const food_item = food_list[j]
+
+   
+    const food_key = `<div class = "food-key">${food_item.key}</div>`
+    const food_values = `<div  class = "food-value">${food_item.value} \xa0 <span> Calories</span></div>`
+    //results.appendChild(element)
+
+    results.innerHTML += food_key
+
+    if ( food_item.key == "Acai Bowl with Fruits"){
+        const food_image =`<div class = "images"><img src = "mealimages/acai-bowl.jpg" class = "image"></img></div>`
+        results.innerHTML += food_image
+    }
+    else if(food_item.key == "Masala Omlette"){
+        const food_image =`<div class = "images"><img src = "mealimages/omelet.jpg" class = "image"></img></div>`
+        results.innerHTML += food_image
+    }
+    else if(food_item.key == "Choco Pancakes with Stawberries"){
+        const food_image =`<div class = "images"><img src = "mealimages/pancake.jpg" class = "image"></img></div>`
+        results.innerHTML += food_image
+    }
+    else if(food_item.key == "Blueberry Smoothie"){
+        const food_image =`<div class = "images"><img src = "mealimages/smoothie.jpg" class = "image"></img></div>`
+        results.innerHTML += food_image
+    }
+    else if(food_item.key == "Sausage and Egg Sandwiches"){
+        const food_image =`<div class = "images"><img src = "mealimages/sandwiches.jpg" class = "image"></img></div>`
+        results.innerHTML += food_image
+    }
+    else if(food_item.key == "Black Bean soup"){
+        const food_image =`<div class = "images"><img src = "mealimages/soup.jpg" class = "image"></img></div>`
+        results.innerHTML += food_image
+    }
+    else if(food_item.key == "Chicken and Farro Salad"){
+        const food_image =`<div class = "images"><img src = "mealimages/chickensalad.jpg" class = "image"></img></div>`
+        results.innerHTML += food_image
+    }
+    else if(food_item.key == "Pot Beef Curry with Rice"){
+        const food_image =`<div class = "images"><img src = "mealimages/beefcurry.jpeg" class = "image"></img></div>`
+        results.innerHTML += food_image
+    }
+    else if(food_item.key == "Bolognese Pasta"){
+        const food_image =`<div class = "images"><img src = "mealimages/bolognesepasta.jpeg" class = "image"></img></div>`
+        results.innerHTML += food_image
+    }
+    else if(food_item.key == "Pork Lettuce Wraps"){
+        const food_image =`<div class = "images"><img src = "mealimages/lettucewrap.jpeg" class = "image"></img></div>`
+        results.innerHTML += food_image
+    }
+    else if(food_item.key == "Rainbow Sushi"){
+        const food_image =`<div class = "images"><img src = "mealimages/ranbowsushi.jpg" class = "image"></img></div>`
+        results.innerHTML += food_image
+    }
+    else if(food_item.key == "Stuffed Butternut Squash"){
+        const food_image =`<div class = "images"><img src = "mealimages/stuffedbutternut.jpg" class = "image"></img></div>`
+        results.innerHTML += food_image
+    }
+    else if(food_item.key == "Straberry and Cream Granola Bar"){
+        const food_image =`<div class = "images"><img src = "mealimages/granola.jpg" class = "image"></img></div>`
+        results.innerHTML += food_image
+    }
+    else if(food_item.key == "BLT Eggs"){
+        const food_image =`<div class = "images"><img src = "mealimages/blt.jpg" class = "image"></img></div>`
+        results.innerHTML += food_image
+    }
+    else if(food_item.key == "PB Choco Cookie"){
+        const food_image =`<div class = "images"><img src = "mealimages/cookies.jpg" class = "image"></img></div>`
+        results.innerHTML += food_image
+    }
+    else if(food_item.key == "Corn Zucchini Fritters"){
+        const food_image =`<div class = "images"><img src = "mealimages/Fritterss.jpg" class = "image"></img></div>`
+        results.innerHTML += food_image
+    }
+    else if(food_item.key == "Prezel"){
+        const food_image =`<div class = "images"><img src = "mealimages/pretzel.jpg" class = "image"></img></div>`
+        results.innerHTML += food_image
+    }
+
+    else{
+        ""
+    }
+   
+    results.innerHTML += food_values
+    const divider = `<hr class="rounded">`
+    results.innerHTML += divider
+   
+  }
+  const calorie_count = `<div class = "count">That's ${count} calories for today, for a better you tomorrow!</div>`
+  results.innerHTML += calorie_count
+  results.innerHTML += `<button class = "button" onclick="window.print()">Print this page</button>`
+  
+  return false
+}
